acbbaa414f2b07292056c9dd28eff399d703de25
sunjunnan79 <2101763682@qq.com>
2025-04-05 16:06:12 +0800

    ignore:修复ignore

--- Muxi_ClassList/internal/data/sql/jxb.sql
+++
@@ -1,9 +0,0 @@
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-CREATE TABLE `jxb`  (
-  `jxb_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `stu_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  UNIQUE INDEX `idx_jxb`(`jxb_id` ASC, `stu_id` ASC) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
-
-SET FOREIGN_KEY_CHECKS = 1;
--- Muxi_ClassList/internal/pkg/pkg.go
+++
@@ -1,8 +0,0 @@
-package pkg
-
-import (
-	"github.com/asynccnu/ccnubox-be/Muxi_ClassList/internal/pkg/crawler"
-	"github.com/google/wire"
-)
-
-var ProviderSet = wire.NewSet(crawler.NewClassCrawler)
