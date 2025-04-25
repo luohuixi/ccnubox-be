8cf03230cfbcb57bd51f3622dd9af26fcc076dc7
sunjunnan <2101763682@qq.com>
2025-04-25 20:34:19 +0800

    feat:增加redis的连接试探

--- bff/ioc/redis.go
+++ bff/ioc/redis.go
@@ -1,6 +1,8 @@
 package ioc
 
 import (
+	"context"
+	"fmt"
 	"github.com/redis/go-redis/v9"
 	"github.com/spf13/viper"
 )
@@ -17,6 +19,12 @@ func InitRedis() redis.Cmdable {
 	if err != nil {
 		panic(err)
 	}
-	//初始化一个redis
-	return redis.NewClient(&redis.Options{Addr: cfg.Addr, Password: cfg.Password})
+	cmd := redis.NewClient(&redis.Options{Addr: cfg.Addr, Password: cfg.Password})
+
+	ctx := context.Background()
+	if err := cmd.Ping(ctx).Err(); err != nil {
+		panic(fmt.Sprintf("Redis 连接失败: %v", err))
+	}
+
+	return cmd
 }
