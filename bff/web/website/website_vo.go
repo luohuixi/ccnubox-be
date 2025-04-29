9caafb6c5fe20b87efb28df337c36d363e7008b7
sunjunnan <2101763682@qq.com>
2025-04-29 10:31:47 +0800

    fix:修复相关静态的id字段可选状态

--- bff/web/website/website_vo.go
+++ bff/web/website/website_vo.go
@@ -3,7 +3,7 @@ package website
 type SaveWebsiteRequest struct {
 	Link        string `json:"link" binding:"required"`
 	Name        string `json:"name" binding:"required"`
-	Id          int64  `json:"id" binding:"required"`
+	Id          int64  `json:"id"` //可选,如果新增记录不用填写
 	Image       string `json:"image" binding:"required"`
 	Description string `json:"description" binding:"required"`
 }
