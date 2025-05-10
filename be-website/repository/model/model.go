06993c55d07a99aa3786d195cca57daa61644c02
sunjunnan <2101763682@qq.com>
2025-05-10 14:27:39 +0800

    feat:增加link的长度限制

--- be-website/repository/model/model.go
+++ be-website/repository/model/model.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Website struct {
 	Name        string `gorm:"column:Name;type:VARCHAR(255);not null"`
-	Link        string `gorm:"column:Link;type:VARCHAR(50)"`
+	Link        string `gorm:"column:Link;type:VARCHAR(255)"`
 	Description string `gorm:"column:Description;type:VARCHAR(255)"`
 	Image       string `gorm:"column:Image;type:VARCHAR(255)"`
 	gorm.Model
