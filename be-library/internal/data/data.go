199b091d981a89d199f204431854bc912caf7fe4
agermel <agermel@mails.ccnu.edu.cn>
2025-08-15 22:06:30 +0800

    ✨ 数据库连接

--- be-library/internal/data/data.go
+++ be-library/internal/data/data.go
@@ -1,50 +1,78 @@
 package data
 
 import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/asynccnu/ccnubox-be/be-library/internal/biz"
 	"github.com/asynccnu/ccnubox-be/be-library/internal/conf"
 	"github.com/redis/go-redis/v9"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	gormLogger "gorm.io/gorm/logger"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
 )
 
 // ProviderSet is data providers.
-var ProviderSet = wire.NewSet(NewData, NewLibraryCrawler)
+var ProviderSet = wire.NewSet(NewData, NewLibraryCrawler, NewSeatRepo, NewDB, NewRedisDB)
 
 // Data 做CURD时使用该框架
 type Data struct {
 	db      *gorm.DB
 	log     *log.Helper
-	crawler *Crawler
+	crawler biz.LibraryCrawler
 	redis   *redis.Client
 }
 
 // NewData .
-func NewData(c *conf.Data, logger log.Logger, db *gorm.DB, crawler *Crawler) (*Data, func(), error) {
+func NewData(c *conf.Data, logger log.Logger, db *gorm.DB, crawler biz.LibraryCrawler) (*Data, error) {
 	data := &Data{
 		log:     log.NewHelper(logger),
 		db:      db,
 		crawler: crawler,
 	}
 
-	cleanup := func() {
-		log.NewHelper(logger).Info("closing the data resources")
-	}
-	return data, cleanup, nil
+	return data, nil
 }
 
-// 迁移
-func (d *Data) migrate() error {
-	d.log.Info("Starting database migration...")
-
-	err := d.db.AutoMigrate(
-		&seat{},
-	)
-
-	if err != nil {
-		return err
+// NewDB 连接 MySQL 数据库并自动迁移
+func NewDB(c *conf.Data) (*gorm.DB, error) {
+	if c == nil {
+		return nil, fmt.Errorf("config data is nil")
 	}
 
-	return nil
+	dsn := c.Database.Source
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: gormLogger.Default.LogMode(gormLogger.Info),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect database: %w", err)
+	}
+
+	if err := db.AutoMigrate(&seat{}, &timeSlot{}); err != nil {
+		return nil, fmt.Errorf("auto migrate failed: %w", err)
+	}
+
+	return db, nil
+}
+
+// NewRedisDB 连接redis
+func NewRedisDB(c *conf.Data, logger log.Logger) *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:         c.Redis.Addr,
+		ReadTimeout:  time.Duration(c.Redis.ReadTimeout.GetSeconds()) * time.Second,
+		WriteTimeout: time.Duration(c.Redis.WriteTimeout.GetSeconds()) * time.Second,
+		DB:           0,
+		Password:     c.Redis.Password,
+	})
+	_, err := rdb.Ping(context.Background()).Result()
+	if err != nil {
+		panic(fmt.Sprintf("connect redis err:%v", err))
+	}
+	log.NewHelper(logger).Info("redis connect success")
+	return rdb
 }
