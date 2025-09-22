25cf72ffee96b658849d732e0e4df96fff3da8f9
sunjunnan <2101763682@qq.com>
2025-09-22 22:08:43 +0800

    feat:修改ES前缀

--- be-class/internal/data/class.go
+++ be-class/internal/data/class.go
@@ -77,7 +77,7 @@ const classMapping = `{
 }
 `
 
-const classIndexName = "ccnubox_class_info"
+const classIndexName = "ccnubox-class_info"
 
 // ProviderSet is data providers.
 var ProviderSet = wire.NewSet(NewClassData, NewEsClient, NewFreeClassroomData, NewRedisClient, NewCache)
